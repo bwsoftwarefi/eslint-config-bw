f645ef625f7a070a61ae394754b0312d2872c7ce
pitkane <mikko@mbit.fi>
2017-10-25 19:49:57 +0300

    Init 1.0.0

---
+++ README.md
@@ -0,0 +1,29 @@
+# eslint-config-bw
+
+## Usage
+
+1. `npm install --save-dev eslint-config-bw eslint`
+
+1. Create .eslintrc file in the root directory
+
+1. And paste this code
+
+```json
+{
+  "extends": "eslint-config-bw"
+}
+```
+
+Define environments if needed
+
+```json
+{
+  "extends": "eslint-config-bw",
+  "rules": {},
+  "env": {
+    "browser": true,
+    "node": true
+  }
+}
+
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+module.exports = {
+  extends: ["airbnb"],
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: "module"
+  },
+  plugins: ["react", "import"],
+  parser: "babel-eslint",
+  rules: {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx"]
+      }
+    ],
+    "react/prefer-stateless-function": "off",
+    "react/forbid-prop-types": [
+      "error",
+      {
+        forbid: []
+      }
+    ],
+    "import/no-named-as-default": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": "off",
+    quotes: [
+      "error",
+      "double",
+      {
+        allowTemplateLiterals: true
+      }
+    ],
+    "arrow-body-style": ["error", "as-needed"],
+    "comma-dangle": [0],
+    "no-console": "off",
+    "import/first": "off",
+    "arrow-parens": "off",
+    "no-underscore-dangle": "off",
+    "consistent-return": "off",
+    "no-param-reassign": "off"
+  },
+  globals: {
+    describe: false,
+    it: false,
+    test: false,
+    expect: false,
+    beforeEach: false,
+    afterEach: false,
+    before: false,
+    window: true
+  }
+};
